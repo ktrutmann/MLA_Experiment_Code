85d6f4a8b0783829e4af79efe1fbe6d1e3810558
ktrutmann <k.trutmann@gmail.com>
2021-11-02 22:30:14 +0100

    Added Ravens Matrices Test with 6 matrices

--- Ravens_Matrices/__init__.py
+++ Ravens_Matrices/__init__.py
@@ -8,14 +8,12 @@ Raven’s Progressive Matrices (RPM)
 Civalli, A. & Deck, S. (2017). A Flexible and Customizable Method for Assessing Cognitive Abilities:
 https://digitalcommons.chapman.edu/cgi/viewcontent.cgi?article=1220&context=esi_working_papers
 """
-# TODO: (2) Add extra matrices!
-# TODO: (3) Translate Instructions!
 
 class Constants(BaseConstants):
     name_in_url = 'RPM'
     players_per_group = None
     num_rounds = 1
-    endowment = 10  # TODO: (2) How much to give?
+    endowment = 10  # TODO: (2) Adjust endowment!
 
 
 class Subsession(BaseSubsession):
@@ -26,89 +24,45 @@ class Group(BaseGroup):
     pass
 
 
-
-def scale_1(label):
-    return models.IntegerField(
-        choices=[
-            [0, 'A'],
-            [1, 'B'],
-            [0, 'C'],
-            [0, 'D'],
-            [0, 'E'],
-            [0, 'F'],
-        ],
-        label=label,
-        widget=widgets.RadioSelectHorizontal,
-    )
-
-def scale_2(label):
-    return models.IntegerField(
-        choices=[
-            [0, 'A'],
-            [0, 'B'],
-            [0, 'C'],
-            [0, 'D'],
-            [1, 'E'],
-            [0, 'F'],
-        ],
-        label=label,
-        widget=widgets.RadioSelectHorizontal,
-    )
-
-def scale_3(label):
-    return models.IntegerField(
-        choices=[
-            [1, 'A'],
-            [0, 'B'],
-            [0, 'C'],
-            [0, 'D'],
-            [0, 'E'],
-            [0, 'F'],
-        ],
-        label=label,
-        widget=widgets.RadioSelectHorizontal,
-    )
-
-
-def scale_4(label):
-    return models.IntegerField(
-        choices=[
-            [1, 'A'],
-            [0, 'B'],
-            [0, 'C'],
-            [0, 'D'],
-            [0, 'E'],
-            [0, 'F'],
-        ],
-        label=label,
-        widget=widgets.RadioSelectHorizontal,
-    )
-
 class Player(BasePlayer):
-    cogn_rpm_matrix_1 = scale_1('Wählen Sie das Element, welches das Bild oben sinnvoll ergänzt:')
-    cogn_rpm_matrix_2 = scale_2('Wählen Sie das Element, welches das Bild oben sinnvoll ergänzt:')
-    cogn_rpm_matrix_3 = scale_3('Wählen Sie das Element, welches das Bild oben sinnvoll ergänzt:')
-    cogn_rpm_matrix_4 = scale_4('Wählen Sie das Element, welches das Bild oben sinnvoll ergänzt:')
+    cogn_rpm_matrix_1 = models.IntegerField(
+        choices=[[0, 'A'], [1, 'B'], [2, 'C'], [3, 'D'], [4, 'E'], [5, 'F']],
+        label='Select the element which best fits the missing picture:',
+        widget=widgets.RadioSelectHorizontal)
+    cogn_rpm_matrix_2 = models.IntegerField(
+        choices=[[0, 'A'], [1, 'B'], [2, 'C'], [3, 'D'], [4, 'E'], [5, 'F']],
+        label='Select the element which best fits the missing picture:',
+        widget=widgets.RadioSelectHorizontal)
+    cogn_rpm_matrix_3 = models.IntegerField(
+        choices=[[0, 'A'], [1, 'B'], [2, 'C'], [3, 'D'], [4, 'E'], [5, 'F']],
+        label='Select the element which best fits the missing picture:',
+        widget=widgets.RadioSelectHorizontal)
+    cogn_rpm_matrix_4 = models.IntegerField(
+        choices=[[0, 'A'], [1, 'B'], [2, 'C'], [3, 'D'], [4, 'E'], [5, 'F']],
+        label='Select the element which best fits the missing picture:',
+        widget=widgets.RadioSelectHorizontal)
+    cogn_rpm_matrix_5 = models.IntegerField(
+        choices=[[0, 'A'], [1, 'B'], [2, 'C'], [3, 'D'], [4, 'E'], [5, 'F']],
+        label='Select the element which best fits the missing picture:',
+        widget=widgets.RadioSelectHorizontal)
+    cogn_rpm_matrix_6 = models.IntegerField(
+        choices=[[0, 'A'], [1, 'B'], [2, 'C'], [3, 'D'], [4, 'E'], [5, 'F']],
+        label='Select the element which best fits the missing picture:',
+        widget=widgets.RadioSelectHorizontal)
     cogn_rpm_total_points = models.IntegerField(initial=None)
-    pers_rpm_overestimation_answer = models.IntegerField(label='Anzahl:', min=0, max=4)
+    pers_rpm_overestimation_answer = models.IntegerField(label='Amount:', min=0, max=6)
     pers_rpm_overestimation_score = models.IntegerField()
-    pers_rpm_overplacement_answer = models.IntegerField(label='Rang:', min=1, max=100)
-
-def get_timeout_seconds(player: Player):
-    return player.participant.vars['expiry'] - time.time()
-
-def is_displayed(player: Player): # TODO: (2) What is happening here?
-    return player.participant.vars['expiry'] - time.time() > 0
+    pers_rpm_overplacement_answer = models.IntegerField(label='Rank:', min=1, max=100)
 
 def add_earnings_to_payoff(player: Player):
-    # Calculate the points:
-    player.cogn_rpm_total_points = player.cogn_rpm_matrix_1 + player.cogn_rpm_matrix_2 +\
-                                        player.cogn_rpm_matrix_3 + player.cogn_rpm_matrix_4
-    player.payoff = c(player.cogn_rpm_total_points * Constants.endowment)
-    player.participant.vars['payoff_dict']['ravens_bonus'] = player.payoff
+    # TODO: (3) Test whether the bonus propagates till the end.
+    answer_key = [1, 5, 4, 3, 0, 0]
+    total_score = 0
 
-    # Update the total payoff
-    player.participant.vars['payoff_dict']['payoff_total'] = player.participant.payoff_plus_participation_fee()
+    for i, this_right_answer in enumerate(answer_key):
+        total_score += getattr(player, f'cogn_rpm_matrix_{i + 1}') == this_right_answer
+    player.cogn_rpm_total_points = total_score
+    player.payoff = cu(player.cogn_rpm_total_points * Constants.endowment)
 
 
 # Pages: ----
@@ -129,81 +83,26 @@ class Introduction_4(Page):
     
 
 class Start(Page):
-    def is_displayed(player: Player):
-        return player.round_number == 1 
-
+    @staticmethod
     def before_next_page(player: Player, timeout_happened):
         # user has 3 minutes to complete as many pages as possible
-        player.participant.vars['expiry'] = time.time() + 3*60
+        player.participant.vars['expiry'] = time.time() + 4.5*60
 
 
-class Matrix_1(Page):
+class Matrix_Page(Page):
     form_model = 'player'
-    form_fields = ['cogn_rpm_matrix_1']
-    timer_text = 'Verbleibende Zeit für die Matrizenaufgaben:'
+    form_fields = ['cogn_rpm_matrix_1', 'cogn_rpm_matrix_2',
+    	'cogn_rpm_matrix_3', 'cogn_rpm_matrix_4',
+        'cogn_rpm_matrix_5', 'cogn_rpm_matrix_6',]
+    timer_text = 'Remaining time:'
+
+    @staticmethod
     def get_timeout_seconds(player: Player):
-        return get_timeout_seconds(player)
+	    return player.participant.vars['expiry'] - time.time()
 
-    def is_displayed(player: Player):
-        return player.round_number == 1 
-
-
-
-class Matrix_2(Page):
-    form_model = 'player'
-    form_fields = ['cogn_rpm_matrix_2']
-    timer_text = 'Verbleibende Zeit für die Matrizenaufgaben:'
-    def get_timeout_seconds(player: Player):
-        return get_timeout_seconds(player)
-
-    def is_displayed(player: Player):
-        return player.round_number == 1 
-
-    def before_next_page(player: Player, timeout_happened, self): # TODO: (2) Test whether the back button still works! -> It does not
-        if self.request.POST.get('Zurück'):
-            if self.request.POST.get('Zurück')[0] == '1':
-                self._is_frozen = False
-                self._index_in_pages -= 2
-                self.participant._index_in_pages -= 2
-
-
-class Matrix_3(Page):
-    form_model = 'player'
-    form_fields = ['cogn_rpm_matrix_3']
-    timer_text = 'Verbleibende Zeit für die Matrizenaufgaben:'
-    def get_timeout_seconds(player: Player):
-        return player.get_timeout_seconds()
-
-    def is_displayed(player: Player):
-        return player.round_number == 1 
-
+    @staticmethod
     def before_next_page(player: Player, timeout_happened):
-        if player.request.POST.get('Zurück'):
-            if player.request.POST.get('Zurück')[0] == '1':
-                player._is_frozen = False
-                player._index_in_pages -= 2
-                player.participant._index_in_pages -= 2
-  
-
-class Matrix_4(Page):
-    form_model = 'player'
-    form_fields = ['cogn_rpm_matrix_4']
-    timer_text = 'Verbleibende Zeit für die Matrizenaufgaben:'
-    def get_timeout_seconds(player: Player):
-        return player.get_timeout_seconds()
-
-    def is_displayed(player: Player):
-        return player.round_number == 1 
-
-    def before_next_page(player: Player, timeout_happened):
-        player.add_earnings_to_payoff()
-
-        if player.request.POST.get('Zurück'):
-            if player.request.POST.get('Zurück')[0] == '1':
-                player._is_frozen = False
-                player._index_in_pages -= 2
-                player.participant._index_in_pages -= 2
-
+        add_earnings_to_payoff(player)
 
 class Overestimation(Page):
     form_model = 'player'
@@ -216,14 +115,11 @@ class Overplacement(Page):
 
 
 page_sequence = [
-    # Introduction_1, # TODO: (3) Uncomment!
-    # Introduction_2,
-    # Introduction_3,
-    # Introduction_4,
+    Introduction_1, 
+    Introduction_2,
+    Introduction_3,
+    Introduction_4,
     Start,
-    Matrix_1,
-    Matrix_2,
-    Matrix_3,
-    Matrix_4,
+    Matrix_Page,
     Overestimation,
     Overplacement]
